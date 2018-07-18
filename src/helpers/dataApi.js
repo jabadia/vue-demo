4575e06decf807d8e6d81ee84349f34e0f622cb3
Javier Abadía <javi.abadia@gmail.com>
2018-07-18 10:25:41 +0200

    _getData() no necesita ser exportada

--- src/helpers/dataApi.js
+++ src/helpers/dataApi.js
@@ -1,27 +1,29 @@
 import axios from 'axios';
+import _ from 'lodash';
 
 const WORLDCUP_DATA_URL = 'https://raw.githubusercontent.com/lsv/fifa-worldcup-2018/master/data.json';
 
 let data = undefined;
 
+function _getData() {
+    return data
+        ? Promise.resolve(data)
+        : axios.get(WORLDCUP_DATA_URL)
+            .then(response => {
+                data = response.data;
+                return data;
+            });
+}
+
 export default {
-    _getData() {
-        return data
-            ? Promise.resolve(data)
-            : axios.get(WORLDCUP_DATA_URL)
-                .then(response => {
-                    data = response.data;
-                    return data;
-                });
-    },
     getTeams() {
-        return this._getData()
+        return _getData()
             .then(data => {
                 return data.teams;
             });
     },
     getMatches() {
-        return this._getData()
+        return _getData()
             .then(data => {
                 const allMatches = _.orderBy(_.flatten(_.concat(
                     _.map(data.groups, 'matches'),
